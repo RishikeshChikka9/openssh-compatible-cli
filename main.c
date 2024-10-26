63f4a0b1c51bf17f75d4b8d718224f72c20eed9a
Chikka Rishikesh <chikkarishi9@gmail.com>
2024-10-26 13:28:30 +0530

    correct some string related code, now perfectly working like an expected cli

--- main.c
+++ main.c
@@ -1,53 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 //parsed tokens are stored in argv array
-char argv[100][100];
+char *argv[100];
 
 //parses the entered input command
-int parser(char *command, int argc){
-    char flag = 0;
-    int i = 0;
-    while(*command){
-        if(*command == ' '){
-            flag++; argc++; command++;
-            i = 0;
-            continue;
-        }
-        argv[flag][i] = *command;
-        command++; i++;
+int parser(char *command) {
+    int argc = 0;
+    char *token = strtok(command, " ");
+    while (token != NULL) {
+        argv[argc++] = token;
+        token = strtok(NULL, " ");
     }
     return argc;
 }
 
 //resets argv to null every time a new command is entered
-void argv_reset(int argc){
-    for(int i = 0; i < argc; i++)
-        for(int j = 0; j < sizeof(argv[i]); j++)
-            argv[i][j] = '\0';
+void argv_reset(int argc) {
+    for (int i = 0; i < argc; i++) {
+        argv[i] = NULL;
+    }
 }
 
 //evaluates the code
-int evaluate(int argc){
-    if(!(strcmp(argv[0], "exit\n"))){
+int evaluate(int argc) {
+    if (!strcmp(argv[0], "exit")) {
         puts("Exiting the code !");
         exit(0);
+    }
+    if (!strcmp(argv[0], "help")) {
+        puts("type 'exit' to exit...");
+        return 0;
+    }
+    pid_t pid = fork();
+    if (!pid) {
+        execvp(argv[0], argv);
+        perror("execvp");
+        exit(1);
+    } else if (pid < 0) {
+        perror("fork");
+    } else {
+        wait(NULL);
+        fflush(stdout);
     }
     return 0;
 }
 
-int main(){
+int main() {
     //variable to store command input
-    char command_input[300];
+    char command_input[256];
 
     //loops through the cli
     puts("Welcome to ssh cli v1.0.0.");
     puts("Type 'help' for more information.");
-    while(1){
+    while (1) {
         printf("> ");
-        fgets(command_input, 300, stdin);
-        int argc = parser(command_input, 0);
+        fgets(command_input, 256, stdin);
+        command_input[strcspn(command_input, "\n")] = '\0'; // Remove newline
+        int argc = parser(command_input);
         evaluate(argc);
         argv_reset(argc);
     }
